31717dee490c52d2bd2b29e6d0b98d9210a0a6e7
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-01-19 21:23:31 +0330

    Feature : File SQL

---
+++ Application.Library/ApplicationContext/Sql Queries/FileGroup/FileGroup_Log.sql
@@ -0,0 +1,53 @@
+﻿IF DB_ID('Performance_Tuning_LogFile_DB') > 0 
+BEGIN
+	ALTER DATABASE Performance_Tuning_LogFile_DB SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE Performance_Tuning_LogFile_DB
+END
+GO
+CREATE DATABASE Performance_Tuning_LogFile_DB
+
+	ON PRIMARY
+	(
+		NAME  = Performance_Tuning_LogFile_DB_Primary,
+		FILENAME = 'C:\Database\Performance_Tuning_LogFile_DB_Primary.mdf',
+		SIZE = 100MB
+	),
+	FILEGROUP FG_Dimension1
+	(
+		NAME  = FG_Dimension1,
+		FILENAME = 'C:\Database\FG_Dimension1.ndf',
+		SIZE = 1GB,
+		FILEGROWTH=1GB
+	),
+	(
+		NAME  = FG_Dimension2,
+		FILENAME = 'C:\Database\FG_Dimension2.ndf',
+		SIZE = 1GB,
+		FILEGROWTH=1GB
+	),
+	FILEGROUP FG_FACT
+	(
+		NAME  = FG_FACT1,
+		FILENAME = 'C:\Database\FG_FACT1.ndf',
+		SIZE = 1GB,
+		FILEGROWTH=1GB
+	),
+	(
+		NAME  = FG_FACT2,
+		FILENAME = 'C:\Database\FG_FACT2.ndf',
+		SIZE = 1GB,
+		FILEGROWTH=1GB
+	)
+	LOG ON
+	(
+		NAME  = Performance_Tuning_LogFile_DB_Log,
+		FILENAME = 'C:\Database\Performance_Tuning_LogFile_DB_Log.ldf',
+		SIZE = 1GB,
+		FILEGROWTH=512MB
+	)
+-----------------------------------------------------
+--ها VLF مشاهده 
+DBCC LOGINFO
+-----------------------------------------------------
+-- مشاهده محتوای لاگ رکورد ها
+SELECT * FROM sys.fn_dblog(null,null)
---
+++ Application.Library/ApplicationContext/Sql Queries/StoredProcedures/FileGroup.sql
@@ -0,0 +1,52 @@
+﻿SP_HELPFILE
+SELECT FILE_ID,NAME,SIZE,MAX_SIZE,GROWTH FROM SYS.Database_Files
+
+IF DB_ID('Performance_TuningDB') > 0 
+BEGIN
+	ALTER DATABASE Performance_TuningDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE Performance_TuningDB
+END
+GO
+
+CREATE DATABASE Performance_TuningDB
+
+	ON PRIMARY
+	(
+		NAME  = Performance_TuningDB_Primary,
+		FILENAME = 'C:\Database\Performance_TuningDB_Primary.mdf',
+		SIZE = 100MB
+	),
+	FILEGROUP FG_Data
+	(
+		NAME  = FG_Data_1,
+		FILENAME = 'C:\Database\FG_Data_1.ndf',
+		SIZE = 1GB,
+		FILEGROWTH=1GB
+	),
+	FILEGROUP FG_Index
+	(
+		NAME  = FG_Index_1,
+		FILENAME = 'C:\Database\FG_Index_1.ndf',
+		SIZE = 1GB,
+		FILEGROWTH=1GB
+	),
+	FILEGROUP FG_Blob
+	(
+		NAME  = FG_Blob_1,
+		FILENAME = 'C:\Database\FG_Blob_1.ndf',
+		SIZE = 1GB,
+		FILEGROWTH=1GB
+	),
+	(
+		NAME  = FG_Blob_2,
+		FILENAME = 'C:\Database\FG_Blob_2.ndf',
+		SIZE = 1GB,
+		FILEGROWTH=1GB
+	)
+	LOG ON
+	(
+		NAME  = Performance_TuningDB_Log,
+		FILENAME = 'C:\Database\Performance_TuningDB_Log.ldf',
+		SIZE = 1GB,
+		FILEGROWTH=512MB
+	)
