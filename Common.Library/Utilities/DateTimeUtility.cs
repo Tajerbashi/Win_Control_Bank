3c031c213e218f8584ce3b60a33d9c3f4680bb33
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-01-27 23:17:28 +0330

    Feature : Add Convertor Gregorian Datetime to Persion date

--- Common.Library/Utilities/DateTimeUtility.cs
+++ Common.Library/Utilities/DateTimeUtility.cs
@@ -1,7 +1,37 @@
-﻿namespace Common.Library.Utilities
+﻿using System.Globalization;
+
+namespace Common.Library.Utilities
 {
     public static class DateTimeUtility
     {
+        public static DateTime ToDateTime(this DateTime dateTime) { return dateTime.ToUniversalTime(); }
+        public static string ToPersion(this DateTime dateTime)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return (string.Format("{0}/{1}/{2}", pc.GetYear(dateTime), pc.GetMonth(dateTime), pc.GetDayOfMonth(dateTime)));
+        }
+        public static string ToPersionFormat(this DateTime dateTime, string format = "yyyy/MM/dd HH:mm:ss")
+        {
+            var calendar = new PersianCalendar();
+            //var persianDate = new DateTime(
+            //    calendar.GetYear(dateTime),
+            //    calendar.GetMonth(dateTime),
+            //    calendar.GetDayOfMonth(dateTime),
+            //    calendar.GetHour(dateTime),
+            //    calendar.GetMinute(dateTime),
+            //    calendar.GetSecond(dateTime));
+            //var result = persianDate.ToString(format, CultureInfo.GetCultureInfo("fa-Ir"));
+            var result = (string.Format("{0}/{1}/{2} {3}:{4}:{5}",
+                calendar.GetYear(dateTime),
+                calendar.GetMonth(dateTime),
+                calendar.GetDayOfMonth(dateTime),
+                calendar.GetHour(dateTime),
+                calendar.GetMinute(dateTime),
+                calendar.GetSecond(dateTime)));
+
+            return result;
+        }
+
     }
-    
+
 }
