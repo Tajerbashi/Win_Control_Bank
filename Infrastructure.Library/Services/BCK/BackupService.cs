bafb40cc3b65f32b9ccc302794e9df185f23c9d2
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-01-28 00:37:32 +0330

    Feature : Add Backup Service

---
+++ Infrastructure.Library/Services/BCK/BackupService.cs
@@ -0,0 +1,21 @@
+﻿using Common.Library.Utilities;
+
+namespace Infrastructure.Library.Services.BCK
+{
+    public sealed class BackupService
+    {
+        public BackupService()
+        {
+            
+        }
+
+        public string BackupDatabase(string path, string name, string DatabaseName)
+        {
+            return ($@"Backup DataBase {DatabaseName} TO DISK ='{path}\{name}_{DateTimeUtility.ToPersion(DateTime.Now)}.bak';");
+        }
+        public string RestoreDatabase(string path, string name, string DatabaseName)
+        {
+            return ($@"Backup DataBase {DatabaseName} TO DISK ='{path}\{name}_{DateTimeUtility.ToPersion(DateTime.Now)}.bak';");
+        }
+    }
+}
