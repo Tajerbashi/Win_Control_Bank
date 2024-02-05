94ac407ea819ba2a3e6569c84479757f02a9b3fc
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-02-06 00:03:03 +0330

    Feature : Domain Test Added

--- Infrastructure.Test/BUS/Bank_Test.cs
+++ Infrastructure.Test/BUS/Bank_Test.cs
@@ -1,6 +1,19 @@
 ﻿namespace Infrastructure.Test.BUS
 {
-    public class Bank_Test
+    public class Bank_Tests
     {
+        [Fact]
+        public void Create_Test()
+        {
+            //  Arrange
+            var a = 10;
+            var b = 20;
+            var c = 30;
+            //  Act
+            var result = a + b + c;
+            //  Assert
+            Assert.Equal(result, 60);
+
+        }
     }
 }
