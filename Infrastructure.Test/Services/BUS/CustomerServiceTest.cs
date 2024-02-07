ec50cfd1fa6ba42c98077d2a9f3b7aaa842b585c
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-02-08 00:04:47 +0330

    Feature : Implmented the Repositories on Services

---
+++ Infrastructure.Test/Services/BUS/CustomerServiceTest.cs
@@ -0,0 +1,8 @@
+﻿using Infrastructure.Test.Repositories.BUS;
+
+namespace Infrastructure.Test.Services.BUS
+{
+    public class CustomerServiceTest : ICustomerRepositoryTest
+    {
+    }
+}
---
+++ Infrastructure.Test/Services/SEC/UserServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Test.Repositories.SEC;
+
+namespace Infrastructure.Test.Services.SEC
+{
+    public class UserServiceTest : IUserRepositoryTest
+    {
+        //private UnitOfWork<EmployeeDBContext> unitOfWork = new UnitOfWork<EmployeeDBContext>();
+        //private GenericRepository<Employee> genericRepository;
+        //private IEmployeeRepository employeeRepository;
+        //public EmployeeController()
+        //{
+        //    //If you want to use Generic Repository with Unit of work
+        //    genericRepository = new GenericRepository<Employee>(unitOfWork);
+        //    //If you want to use a Specific Repository with Unit of work
+        //    employeeRepository = new EmployeeRepository(unitOfWork);
+        //}
+    }
+}
