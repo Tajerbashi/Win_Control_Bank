5291ea252cad03c9a7343392230cfa2c9fa499ce
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-02-11 00:47:43 +0330

    Feature : How to Read External Files

---
+++ Infrastructure.Test/DataContext/BUS/Customer/DataClassCustomer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace Infrastructure.Test.DataContext.BUS.Customer
+{
+    public class DataClassCustomer : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 1000, true };
+            yield return new object[] { 2000, false };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+}
---
+++ Infrastructure.Test/DataContext/BUS/Customer/DataType.cs
@@ -0,0 +1,21 @@
+﻿namespace Infrastructure.Test.DataContext.BUS.Customer
+{
+    //  Usage
+    //  [ClassData(typeof(DataClassCustomer))]
+
+    public class DataType
+    {
+
+        //  Inline Data
+        //  [InlineData(p1,p2)]
+
+        //  Member Data
+        //  [MemberData(p1,p2,...)]
+
+        //  Class Data
+        //  [ClassData("")]
+
+    }
+
+
+}
